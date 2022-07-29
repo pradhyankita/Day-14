8d0bde058f50713a3473e0e652b66e301e02a51b
pradhyankita <rapenzelpb@gmail.com>
2022-07-29 10:18:23 +0530

    adding-UC3

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -2,16 +2,12 @@ package com.bridgelabz.linkedlist;
 
 public class LinkedList<K> {
     public class INode <T>{
-
         T data;
         INode<T> next;
 
-        public INode() {
-        }
 
         public INode(T data) {
             this.data = data;
         }
-
     }
 }
