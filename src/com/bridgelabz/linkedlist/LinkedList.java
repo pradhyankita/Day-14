3b212cbcaca030c9846b51f83d3b29e90ae15a75
pradhyankita <104745890+pradhyankita@users.noreply.github.com>
2022-07-29 10:30:16 +0530

    Update LinkedList.java

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,13 +1,41 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedList<K> {
-    public class INode <T>{
-        T data;
-        INode<T> next;
+public class LinkedList<T> {
+    INode<T> head;
+    INode<T> tail;
 
-
-        public INode(T data) {
-            this.data = data;
+    public LinkedList(){
+        this.head=null;
+        this.tail=null;
+    }
+    void push(T data){
+        INode<T> node = new INode<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next = head;
+            head = node;
         }
     }
+    public void add(T data) {
+        INode<T> node = new INode<>(data);
+        if (head == null){
+            head = node;
+        }
+        else{
+            tail.next = node;
+        }
+        tail = node;
+    }
+    void print(){
+        INode<T> temp = head;
+        while (temp != null){
+            System.out.print(temp.data+ " ");
+=
+           temp = temp.next;
+        }
+    }
+
 }
