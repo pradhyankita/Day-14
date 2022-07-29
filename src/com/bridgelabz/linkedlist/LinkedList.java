67b6befda64476f34564896f7e684a4ca5ec4c87
pradhyankita <104745890+pradhyankita@users.noreply.github.com>
2022-07-29 10:29:15 +0530

    Update LinkedList.java

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,13 +1,72 @@
 package com.bridgelabz.linkedlist;
 
-public class LinkedList<K> {
-    public class INode <T>{
-        T data;
-        INode<T> next;
+public class LinkedList<T> {
+    INode<T> head;
+    INode<T> tail;
 
-
-        public INode(T data) {
-            this.data = data;
+    public LinkedList(){
+        this.head=null;
+        this.tail=null;
+    }
+     public void push(T data){
+        INode<T> node = new INode<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next = head;
+            head = node;
         }
     }
+    public void add(T data) {
+        INode<T> node = new INode<>(data);
+        if (head == null){
+            head = node;
+        }
+        else{
+            tail.next = node;
+        }
+        tail = node;
+    }
+   public void print(){
+        INode<T> temp = head;
+        while (temp != null){
+            System.out.print(temp.data+ " ");
+
+           temp = temp.next;
+        }
+    }
+    public void insert(T data) {
+        INode<T> node = new INode<>(data);
+        head.next = node;
+        node.next = tail;
+    }
+
+    public T pop() {
+        T deletedElement = head.data;
+        head = head.next;
+        return deletedElement;
+    }
+    public T popLastElement() {
+        T deletedElement = tail.data;
+        INode<T> temp = head;
+        while(temp.next != tail){
+            temp = temp.next;
+        }
+        temp.next = null;
+        temp = tail;
+        return  deletedElement;
+    }
+
+
+    public T peek() {
+
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
 }
