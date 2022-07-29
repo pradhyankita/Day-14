03e8ebaf921896a602de8f637704648d342c7dfd
pradhyankita <rapenzelpb@gmail.com>
2022-07-29 10:21:00 +0530

    adding-UC5

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -4,8 +4,7 @@ public class LinkedList<K> {
     public class INode <T>{
         T data;
         INode<T> next;
-
-
+        
         public INode(T data) {
             this.data = data;
         }
